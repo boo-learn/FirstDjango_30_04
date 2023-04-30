d95cab43bcf776c683c6d1cbecd3be25177e1c85
boo <eyurchenko@specialist.ru>
2023-04-30 18:17:45 +0700

    Module-1 practice task-2 complete

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render, HttpResponse
 
+author = {
+    "name": "Евгений",
+    "surname": "Юрченко",
+    "phone": "89007001122",
+    "email": "eyurchenko@specialist.ru",
+}
+items = [
+    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
+    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
+    {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
+    {"id": 7, "name": "Картофель фри", "quantity": 0},
+    {"id": 8, "name": "Кепка", "quantity": 124},
+]
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello!!")
+    text = """
+    <h1>"Изучаем django"</h1>
+    <strong>Автор</strong>: <i>Иванов И.П.</i>
+    """
+    return HttpResponse(text)
 
 
 def about(request):
-    return HttpResponse("Юрченко Евгений Витальевич")
+    text = f"""
+    Имя: <b>{author['name']}</b><br>
+    Фамилия: <b>{author['surname']}</b><br>
+    телефон: <b>{author['phone']}</b><br>
+    email:   <b>{author['email']}</b>
+    """
+    return HttpResponse(text)
