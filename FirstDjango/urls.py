64bc3b9e57b07f5534338a11650f004b6049c107
boo <eyurchenko@specialist.ru>
2023-04-30 17:13:06 +0700

    demo-2 (about)

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -4,4 +4,5 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.home),
+    path('about/', views.about),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Hello!!")
+
+
+def about(request):
+    return HttpResponse("Юрченко Евгений Витальевич")
