a90095feeb7e178de452ce23f52d7c31a2f3f754
boo <eyurchenko@specialist.ru>
2023-05-07 20:17:06 +0700

    add static files

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home),
-    path('about/', views.about),
-    path('item/<int:id>/', views.item_page),
-    path('items/', views.items_list),
-]
+    path('home/', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('item/<int:id>/', views.item_page, name="item-detail"),
+    path('items/', views.items_list, name="items-list"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
