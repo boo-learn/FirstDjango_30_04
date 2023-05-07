45b17eace579a4ae6665cc8e20bad9f238e87af9
boo <eyurchenko@specialist.ru>
2023-05-07 16:08:06 +0700

    add Model Item

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    count = models.PositiveIntegerField()
+
+    def __repr__(self):
+        return f"Item: {self.name} | {self.brand}"
